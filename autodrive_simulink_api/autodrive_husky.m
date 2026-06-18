8d03592fbe5a7c7163d545032b83d25f3e97e28c
Tinker Twins <tinkertwinsofficial@gmail.com>
2026-06-18 17:22:26 -0400

    Change block sample time

--- autodrive_simulink_api/autodrive_husky.m
+++ autodrive_simulink_api/autodrive_husky.m
@@ -147,7 +147,7 @@ block.NumDialogPrms = 1;
 % [positive_num offset] : Discrete sample time
 % [-1, 0] : Inherited sample time
 % [-2, 0] : Variable sample time
-block.SampleTimes = [1 0];
+block.SampleTimes = [0 0];
 
 % Specify the block simStateCompliance. The allowed values are:
 % 'UnknownSimState', < The default setting; warn and assume DefaultSimState
